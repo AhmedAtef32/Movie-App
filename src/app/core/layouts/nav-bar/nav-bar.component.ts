28f950c9a91508874b62a7c0b77aabc405020b3d
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-07-06 00:26:57 +0300

    ADD NavBar

--- src/app/core/layouts/nav-bar/nav-bar.component.ts
+++ src/app/core/layouts/nav-bar/nav-bar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink , RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive , DrawerModule, ButtonModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrl: './nav-bar.component.css',
 })
 export class NavBarComponent {
+  @ViewChild('navbar') navbar!: ElementRef<HTMLElement>;
+  visible1: boolean = false;
 
+
+
+  @HostListener('window:scroll')
+  /**
+   * Adjusts the background color of the navigation bar based on the window scroll position.
+   * If the scroll position is greater than 50, a semi-transparent background is added.
+   * Otherwise, the background is removed.
+   */
+  onWindowScroll() {
+    let nav = this.navbar.nativeElement;
+    let scrollPosition = window.scrollY;
+
+    if (scrollPosition > 50) {
+      nav.classList.add('bg-[rgba(12,19,33,0.9)]!');
+    } else {
+      nav.classList.remove('bg-[rgba(12,19,33,0.9)]!');
+    }
+  }
 }
--- src/app/featured/pages/home/components/main-slider/main-slider.component.ts
+++ src/app/featured/pages/home/components/main-slider/main-slider.component.ts
@@ -1,4 +1,14 @@
-import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  signal,
+  viewChild,
+  ViewChild,
+  ViewChildren,
+  WritableSignal,
+} from '@angular/core';
 import { ItrendingMovie } from '../../../../interfaces/itrending-movie';
 import { MoivesService } from '../../../../services/moive/moives.service';
 import { CarouselModule } from 'ngx-owl-carousel-o';
@@ -8,78 +18,79 @@ import { TermPipe } from '../../../../../core/pipe/term.pipe';
 
 @Component({
   selector: 'app-main-slider',
-  imports: [CarouselModule, DatePipe ,TermPipe],
+  imports: [CarouselModule, DatePipe, TermPipe],
   templateUrl: './main-slider.component.html',
-  styleUrl: './main-slider.component.css'
+  styleUrl: './main-slider.component.css',
 })
 export class MainSliderComponent implements OnInit {
-
   private readonly _movieService = inject(MoivesService);
 
   responsiveOptions: any[] | undefined;
-  trendingMoive!:ItrendingMovie[]
-  pathImage:string = this._movieService.PathImageUrl
-  movieNumber:WritableSignal<number> = signal(1)
-  counter:WritableSignal<number> = signal(1)
+  trendingMoive!: ItrendingMovie[];
+  pathImage: string = this._movieService.PathImageUrl;
+  movieNumber: WritableSignal<number> = signal(1);
+  counter: WritableSignal<number> = signal(1);
+  @ViewChildren('movieDetails') movieDetails!: ElementRef<HTMLElement>[];
+  ngOnInit() {
+    this.getTrendingMovies();
+  }
 
-ngOnInit() {
-  this.getTrendingMovies()
-}
-
-
-
-getTrendingMovies(){
-   this._movieService.getTrendingMovies().subscribe({
-      next : (res)=>{
-        this.trendingMoive = res.results
-        this.movieNumber.set(this.trendingMoive.length)
-
+  getTrendingMovies() {
+    this._movieService.getTrendingMovies().subscribe({
+      next: (res) => {
+        this.trendingMoive = res.results;
+        this.movieNumber.set(this.trendingMoive.length);
       },
-      error : (err)=>{
-        console.log(err)
-      }
+      error: (err) => {
+        console.log(err);
+      },
     });
-}
+  }
 
-customOptions: OwlOptions = {
+  customOptions: OwlOptions = {
     loop: true,
     mouseDrag: false,
     touchDrag: false,
     pullDrag: false,
+    dots: false,
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',
-    dots: false,
     margin: 20,
     navSpeed: 700,
     navText: ['', ''],
     responsive: {
       0: {
-        items: 1
-      }
+        items: 1,
+      },
     },
     nav: false,
+  };
 
-  }
-
-  nextBtn(){
-    if(this.counter() == this.movieNumber()){
-      this.counter.set(1)
-    }else{
-
-      this.counter.update((counter) => counter + 1)
+  nextBtn() {
+    if (this.counter() == this.movieNumber()) {
+      this.counter.set(1);
+    } else {
+      this.counter.update((counter) => counter + 1);
     }
   }
 
-  prevBtn(){
-    if(this.counter() == 1){
-      this.counter.set(this.movieNumber())
-    }else{
-
-      this.counter.update((counter) => counter - 1)
+  prevBtn() {
+    if (this.counter() == 1) {
+      this.counter.set(this.movieNumber());
+    } else {
+      this.counter.update((counter) => counter - 1);
     }
-
   }
 
+  onTranslated() {
+    this.movieDetails.forEach((ele) => {
+      ele.nativeElement.classList.remove('animate__fadeInUp');
+      ele.nativeElement.classList.add('opacity-0');
 
-
+      setTimeout(() => {
+        ele.nativeElement.classList.add('opacity-100');
+        ele.nativeElement.classList.add('animate__fadeInUp');
+      }, 30);
+    });
+  }
 }
