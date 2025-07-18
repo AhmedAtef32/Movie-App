29ef667f797c18d6f2ff75a23f022f84e9656274
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-07-19 01:51:55 +0300

    ADD Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { MainSliderComponent } from "./featured/pages/home/components/main-slider/main-slider.component";
-import { HomeComponent } from "./featured/pages/home/home.component";
-import { NavBarComponent } from "./core/layouts/nav-bar/nav-bar.component";
+import { MainSliderComponent } from './featured/pages/home/components/main-slider/main-slider.component';
+import { HomeComponent } from './featured/pages/home/home.component';
+import { NavBarComponent } from './core/layouts/nav-bar/nav-bar.component';
+import { FooterComponent } from './core/layouts/footer/footer.component';
+import { ScrollTop } from 'primeng/scrolltop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, MainSliderComponent, HomeComponent, NavBarComponent],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    MainSliderComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
+    ScrollTop,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'movieApp';
